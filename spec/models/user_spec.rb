7db4db494ee0982d18b7c09bbf33673a27a49153
Al Snow <jasnow@hotmail.com>
2011-05-14 16:55:37 -0400

    87 green tests on page 404

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -133,6 +133,24 @@ describe User do
       wrong_password_user = User.authenticate(@attr[:email], @attr[:password])
       wrong_password_user.should @user
     end
+  end
 
+  describe "admin attribute" do
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+
+    it "should respond to admin" do
+      @user.should respond_to(:admin)
+    end
+
+    it "should not be an admin by default" do
+      @user.should_not be_admin
+    end
+
+    it "should be convertible to an admin" do
+      @user.toggle(:admin) # true <=> false
+      @user.should be_admin
+    end
   end
 end
