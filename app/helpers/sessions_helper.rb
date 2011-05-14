21eae82df80a7cba60b0e8f68b22dff5ecdbe976
Al Snow <jasnow@hotmail.com>
2011-05-14 15:11:30 -0400

    80 green tests

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -28,10 +28,24 @@ module SessionsHelper
   end
 
   def deny_access
-    redirect_to signin_path, :notice => "Please sign in to acess this page."
+    store_location
+    redirect_to signin_path, :notice => "Please sign in to access this page."
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
   end
 
   private
+
+    def store_location
+      session[:return_to] = request.fullpath
+    end
+
+    def clear_return_to
+      session[:return_to] = nil
+    end
 
     def user_from_remember_token
       User.authenticate_with_salt(*remember_token)
