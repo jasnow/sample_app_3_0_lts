a78a6a7291c3a22da1de58df14d707ba3cd37818
Al Snow <jasnow@hotmail.com>
2011-05-21 13:06:28 -0400

    page 507 ; 151 green tests

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+# Schema version: 20110521134254
+#
+# Table name: microposts
+#
+#  id         :integer         not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Micropost < ActiveRecord::Base
   attr_accessible :content
 
@@ -5,6 +17,12 @@ class Micropost < ActiveRecord::Base
 
   validates :content, :presence => true, :length => { :maximum => 140 }
   validates :user_id, :presence => true
+
+#TODO;
+#  def self.from_user_followed_by(user)
+#    followed_ids = user.following.map(&:id).join(" ")
+#    where('user_id IN (#{followed_ids}) OR user_id = ?', user) 
+#  end
 
   default_scope :order => 'microposts.created_at DESC'
 end
